aaae444d0f014e7b1c0507a2d33e598c52709521
Mehmet-D02 <71323491+Mehmet-D02@users.noreply.github.com>
2023-08-18 19:24:58 +0300

    Delete ToDoAppProject directory

--- ToDoAppProject/todoapp/todolist/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("update/<int:pk>/", views.update_task, name="update_task"),
-    path("delete/<int:pk>/", views.delete_task, name="delete_task"), #this
-]
